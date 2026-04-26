e5d8ad216e2e54b3a2c475ee012dc144e3e7490e
Erik Schuessler <erikmaloschuessler@gmail.com>
2026-04-25 23:17:52 -0600

    add the functions to generate random colors

--- app.js
+++ app.js
@@ -3,6 +3,23 @@ window.onload = function () {
     const paletteSize = document.getElementById("palette-size");
     const generateBtn = document.getElementById("generate-btn");
     const paletteContainer = document.getElementById("palette-container");
-
-
+    
+    //Random HSL generator
+    const getRandomNumber = (min, max) => {
+        return Math.floor(Math.random() * (max - min + 1)) + 1;
+    }
+    const hue = getRandomNumber(0, 360);
+    const saturation = getRandomNumber(0, 100);
+    const lightness = getRandomNumber(0, 100);
+    let hslColor = `hsl(${hue} ${saturation}% ${lightness}%)`;
+    
+    // Random HEX generator
+    const hexValues = "0123456789ABCDEF";
+    const getHexColor = (values) => {
+        let hex = "#";
+        for (let i = 0; i < 6; i++) {
+            hex += values[Math.floor(Math.random() * values.length)]
+        }
+        return hex;
+    };
 }
