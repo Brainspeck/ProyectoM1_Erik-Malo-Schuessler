17b90446ef17cd73a9d8751b6501db7cb9746beb
Erik Schuessler <erikmaloschuessler@gmail.com>
2026-04-25 21:23:16 -0600

    add basic html and js structure

---
+++ app.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    
+    const paletteSize = document.getElementById("palette-size");
+    const generateBtn = document.getElementById("generate-btn");
+    const paletteContainer = document.getElementById("palette-container");
+
+
+}
